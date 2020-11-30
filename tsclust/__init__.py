8b11da0366194059ec02f38ea0d7bfa8a7602688
imartinezl <inigomlap@gmail.com>
2020-11-30 11:51:45 +0100

    Source code example

---
+++ tsclust/__init__.py
@@ -0,0 +1,3 @@
+__author__ = """Iñigo Martinez"""
+__email__ = 'inigomlap@gmail.com'
+__version__ = '0.1.0'
---
+++ tsclust/tsclust.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def add_one(number):
+    return number + 1
+
+def create_array(n):
+    return np.array([n])
