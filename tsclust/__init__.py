5a1033581fd73d0055c9cb76d5e8c3aac3f283c7
imartinezl <inigomlap@gmail.com>
2020-11-30 13:01:23 +0100

    entry point: main

--- tsclust/__init__.py
+++ tsclust/__init__.py
@@ -1,3 +1,7 @@
 __author__ = """Iñigo Martinez"""
 __email__ = 'inigomlap@gmail.com'
 __version__ = '0.1.0'
+
+def main():
+    """Entry point for the application script"""
+    print("Call your main application code here")
