4b021ad4f54d9bb42c42ffe12eda8d7a56f8246c
imartinezl <inigomlap@gmail.com>
2020-12-02 10:12:08 +0100

    v0.1.1

--- tsclust/__init__.py
+++ tsclust/__init__.py
@@ -2,6 +2,7 @@ __author__ = """Iñigo Martinez"""
 __email__ = "inigomlap@gmail.com"
 __version__ = "0.1.1"
 
+from .example import add_one
 
 def main():
     """Entry point for the application script"""
